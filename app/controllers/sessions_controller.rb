973d678d677daecea24157350ab20c12c8d5f72d
reeses-pieces <nicholasreis@icloud.com>
2016-10-15 16:14:18 -0700

    minor layout fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ get '/sessions/new' do
   erb :'sessions/new'
 end
 
-
 post '/sessions' do
   @user = User.find_by_email(params[:email])
 
@@ -16,8 +15,6 @@ post '/sessions' do
     erb :'sessions/new'
   end
 end
-
-
 
 # delete '/sessions/:id' do
 delete '/sessions' do
