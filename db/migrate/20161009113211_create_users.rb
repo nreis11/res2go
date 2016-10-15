1259cf0fa3fead1dd9a60ee01c7271aa913a4a84
reeses-pieces <nicholasreis@icloud.com>
2016-10-15 15:34:07 -0700

    reset to has many through

--- db/migrate/20161009113211_create_users.rb
+++ db/migrate/20161009113211_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
+      t.string :phone
       t.string :password_hash
       
       t.timestamps null:false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 users = 5.times do
   User.create!( :username  => Faker::Internet.user_name,
                 :email      => Faker::Internet.email,
+                :phone => Faker::PhoneNumber,
                 :password   => 'password' )
 end
 
