30b6697532a3d933b81005b017b12020dd1dc65d
reeses-pieces <nicholasreis@icloud.com>
2016-10-16 11:55:35 -0700

    add reservoir to user working

--- app/controllers/reservoirs_controller.rb
+++ app/controllers/reservoirs_controller.rb
@@ -3,7 +3,33 @@ get '/reservoirs' do
   erb :"/reservoirs/index"
 end
 
+post '/reservoirs' do
+  p params
+  reservoir_id = Reservoir.find_by(name: params["reservoir_name"]).id
+  p reservoir_id
+  @stat = Stat.new(user_id: current_user.id,
+                   reservoir_id: reservoir_id,
+                   floor_capacity: params["floor_capacity"],
+                   ceiling_capacity: params["ceiling_capacity"]
+                   )
+  if @stat.save
+    redirect "/users/#{current_user.id}"  
+  else
+    @errors = @stat.errors.full_messages
+    erb :"/reservoirs/new"
+  end
+
+end
+
+get '/reservoirs/new' do
+  @user = current_user
+  @reservoirs = Reservoir.all
+  erb :"/reservoirs/new"
+end
+
 get '/reservoirs/:id' do
   # Show reservoir
   erb :"/reservoirs/show"
 end
+
+
