186fc84d936237094d540364fafe84f545bce7ca
reeses-pieces <nicholasreis@icloud.com>
2016-10-16 22:07:14 -0700

    clean up web scraper

--- app/models/http_parser/web_scraper.rb
+++ app/models/http_parser/web_scraper.rb
@@ -4,7 +4,12 @@ require 'JSON'
 require 'pry'
 require 'csv'
 
-reservoir_page = HTTParty.get("http://cdec.water.ca.gov/cgi-progs/reservoirs?s=RES")
+# USE REAL URL WHEN READY
+# reservoir_page = HTTParty.get("http://cdec.water.ca.gov/cgi-progs/reservoirs?s=RES")
+
+# SCRAPING FROM FILE TEMPORARILY
+file = File.open('reservoirs.html', 'r')
+reservoir_page = file.read
 
 noko_res = Nokogiri::HTML(reservoir_page)
 
@@ -20,16 +25,41 @@ headers = headers_text.split("\n").map { |header| header }
 # Remove newline in first index
 headers.delete_at(0)
 
-reservoir_names = main_content.css('tr').css('td').first.text
-reservoir_name = main_content.css('tr')[4].css('td').first.text
+# reservoir_names = main_content.css('tr').css('td').first.text
+# reservoir_name = main_content.css('tr')[4].css('td').first.text
 
-table_data = table_data_children.map do |child_index|
-  data_point = main_content.css('tr')[4].css("td:nth-child(#{child_index})").text
-  data_point
+# table_data = table_data_children.map do |child_index|
+#   data_point = main_content.css('tr')[4].css("td:nth-child(#{child_index})").text
+#   data_point
+# end
+
+table_rows = main_content.css('tr')
+
+rows = []
+table_rows.each do |row|
+  table_data = table_data_children.map do |child_index|
+    data_point = row.css("td:nth-child(#{child_index})").text
+    data_point.strip
+  end
+  rows << table_data
+end
+
+rows.delete_if { |row| row.any? { |value| value.length < 3 } }
+rows.delete_at(0)
+
+# table_rows = table_rows.keep_if { |row| row.count > 10 }
+headers = ["name", "capacity", "storage"]
+
+CSV.open("reservoir_data.csv", 'w') do |csv|
+  csv << headers
+  rows.each do |row|
+    csv << row
+  end
 end
 
 
 
 
 
-Pry.start(binding)
+
+# Pry.start(binding)
